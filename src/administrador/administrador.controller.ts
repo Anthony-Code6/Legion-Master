d19c4649351034a6e163ad4b1eb5737e20078b77
Anthony Enrique Velasquez Honorio <anthony2000_honorio@outlook.com>
2025-03-06 01:35:26 -0500

    Proyecto Completado

--- src/administrador/administrador.controller.ts
+++ src/administrador/administrador.controller.ts
@@ -1,12 +1,70 @@
-import { BadRequestException, Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Res } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Res, UseGuards } from '@nestjs/common';
 import { AdministradorService } from './administrador.service';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 import { Response } from 'express';
+import { AuthGuard } from 'src/core/guards/auth/auth.guard';
+import { RolesGuard } from 'src/core/guards/roles-auth/roles-auth.guard';
+import { User } from 'src/core/decorator/user/user.decorator';
 
+@ApiBearerAuth()
+@UseGuards(AuthGuard, RolesGuard)
 @Controller('administrador')
 export class AdministradorController {
     constructor(private readonly adminServices: AdministradorService,
     ) { }
+
+
+    @Get('/admin-dashboard')
+    @ApiOperation(
+        {
+            summary: 'Lista informacion ',
+            description: `
+            Types
+            {
+                "_tareas_trabajo": {},
+                _cantidad_registros:{},
+                "exito": boolean,
+                "mensajeError": string,
+                "mensaje": string
+            }
+    
+            Description
+            {    
+                _total_tareas: Lista el total de tareas por trabajo ,
+                _cantidad_registros:Lista la informacion para la grafica
+                exito: Indicador de éxito  
+                mensajeError: Mensaje de error,
+                mensaje: Mensaje
+            } 
+            `
+        }
+    )
+    async Admin_Trabajo_TotalTareas(@Res() res: Response, @User('sub') sub: string) {
+        try {
+
+            var total_trabajo_tareas = await this.adminServices.Admin_Trabajo_TotalTareas(sub)
+            var cantidad_por_areas = await this.adminServices.Admin_Total_Areas(sub)
+            return res.status(HttpStatus.OK).json({
+                exito: true,
+                mensajeError: '',
+                mensaje: '',
+                _tareas_trabajo: total_trabajo_tareas,
+                _cantidad_registros: cantidad_por_areas
+            });
+        } catch (err) {
+            if (err instanceof BadRequestException) {
+                return res.status(HttpStatus.BAD_REQUEST).json({
+                    exito: false,
+                    mensajeError: err.message,
+                });
+            }
+
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+                exito: false,
+                mensajeError: 'Ocurrió un error inesperado.',
+            });
+        }
+    }
 
     
 }
--- src/administrador/administrador.service.ts
+++ src/administrador/administrador.service.ts
@@ -1,6 +1,87 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AdministradorService {
+    constructor(private prismaServices: PrismaService) { }
 
+    async Admin_Trabajo_TotalTareas(usuarioid: string) {
+        try {
+            const trabajos_total_tareas = await this.prismaServices.trabajos.findMany(
+                {
+                    where: {
+                        idUsuario: usuarioid
+                    },
+                    select: {
+                        nombre: true,
+                        Tareas: {
+                            select: {
+                                estado: true
+                            }
+                        },
+                        _count: {
+                            select: { Tareas: true }
+                        }
+                    },
+                    orderBy:{
+                        nombre:'asc'
+                    }
+                }
+            )
+
+            // Procesar los datos para contar tareas en estado true y false
+            return trabajos_total_tareas.map(trabajo => {
+                const total = trabajo._count.Tareas;
+                const completado = trabajo.Tareas.filter(t => t.estado).length;
+                const pendiente = total - completado;
+
+                return {
+                    nombre: trabajo.nombre,
+                    total,
+                    completado,
+                    pendiente
+                };
+            });
+        } catch (err) {
+            throw err
+        }
+    }
+
+    async Admin_Total_Areas(usuarioid: string) {
+        try {
+
+            const notas = await this.prismaServices.usuarios.findUnique(
+                {
+                    where: { idUsuarios: usuarioid },
+                    select: {
+                        _count: {
+                            select: { Notas: true }
+                        }
+                    }
+                }
+            )
+
+            const proyectos = await this.prismaServices.usuarios.findUnique(
+                {
+                    where: { idUsuarios: usuarioid },
+                    select: {
+                        _count: {
+                            select: { Proyectos: true }
+                        }
+                    }
+                }
+            )
+
+            const labels = ['Notas', 'Proyectos']
+            const data = [notas?._count.Notas, proyectos?._count.Proyectos]
+            
+            // Procesar los datos para contar tareas en estado true y false
+            return {
+                labels: labels,
+                data: data
+            }
+        } catch (err) {
+            throw err
+        }
+    }
 }
